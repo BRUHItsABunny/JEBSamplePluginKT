4eed1d881741920e3335de6fcc2c59fc7d60ab83
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2022-06-24 22:29:55 +0200

    Step 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 }
 
 dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.4.2")
+
     testImplementation(kotlin("test"))
 }
 
@@ -21,4 +27,15 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes["JebPlugin-entryclass"] = "com.bunnytechsolutions.jebsamplepluginkt.PluginEntry"
+        attributes["jebPlugin-version"] = project.version
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
+        exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+    }
 }
